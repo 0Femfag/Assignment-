01b8747f60a1a92e70b71d50d0817987b99508d2
0Femfag <Femimane1@gmail.com>
2024-08-16 02:12:40 +0100

    Add files via upload

--- user.js
+++ user.js
@@ -1,54 +1,15 @@
-const userModel = require("../models/user");
+const express = require("express");
+const {
+  createUser,
+  getUsers,
+  updateUser,
+  deleteUser,
+} = require("../controllers/user");
+const routes = express.Router();
 
-const createUser = async function (req, res) {
-  const body = req.body;
-  console.log(body);
-  try {
-    const newUser = new userModel(body);
-    const savedUser = await newUser.save();
-    console.log(savedUser);
-    res.json(savedUser);
-  } catch (error) {
-    console.log(error);
-    res.json({ message: "user already exist" });
-  }
-};
+routes.post("/get-all-users", createUser);
+routes.get("/get-all-users", getUsers);
+routes.put("/get-all-user", updateUser);
+routes.delete("/get-all-user", deleteUser);
 
-const getUsers = async (req, res) => {
-  try {
-    const users = await userModel.find().sort({ username: -1 });
-    res.json(users);
-  } catch (error) {
-    res.json({ message: error.message });
-  }
-};
-
-const updateUser = async function (req, res) {
-  const { id, username } = req.body;
-  console.log(id);
-  try {
-    const updatedUser = await userModel.findByIdAndUpdate(
-      id,
-      { username },
-      { new: true }
-    );
-    console.log(updatedUser);
-    res.json(updatedUser);
-  } catch (error) {
-    res.json({ message: error.message });
-  }
-};
-
-const deleteUser = async (req, res) => {
-  const { id } = req.body;
-  console.log(id);
-  try {
-    const deletedUser = await userModel.findByIdAndDelete(id);
-    console.log(deletedUser);
-    res.json(deletedUser);
-  } catch (error) {
-    res.json({ message: error.message });
-  }
-};
-
-module.exports = { createUser, getUsers, updateUser, deleteUser };
+module.exports = routes;
